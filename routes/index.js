9062b2b029718acb5766b3b92c821c7c470fc51a
arnont <nongnont.170442@gmail.com>
2021-10-03 22:03:50 +0700

    connect database test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL='postgresql://tfjupltyynvjwa:0d20ce307bedc98f3f5d4ce740b26c187e21d468f94055a8761ed46e0f5e009e@ec2-44-198-196-149.compute-1.amazonaws.com:5432/dbbufi8tv9mjkh',
   ssl: {
     rejectUnauthorized: false
   }
@@ -17,7 +17,7 @@ router.get('/', function (req, res, next) {
 router.get('/db', async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
+    const result = await client.query('SELECT * FROM users');
     const results = { 'results': (result) ? result.rows : null };
     res.send(JSON.stringify(results));
     client.release();
