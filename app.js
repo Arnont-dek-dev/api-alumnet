4eac8d8b9fdd4948c1c7377ac4a1f3dd664c862e
arnont <nongnont.170442@gmail.com>
2021-10-05 03:01:28 +0700

    test insert

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 var cors = require('cors');
 
 var indexRouter = require('./routes/index');
